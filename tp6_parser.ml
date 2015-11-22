9ac0e7c56f1235edeb9d5b5e50fc4222b037d1f4
Anthony Geourjon <anthony@geourjoa-desktop>
2015-11-22 21:44:20 +0100

    Ajout du TP6
    TP6 sur l’analyse syntaxique, les parties facultatives n'ont pas été faites

--- tp6_parser.ml
+++ tp6_parser.ml
@@ -1,20 +1,72 @@
-(* TP7 OCaml 
+(* --------------------------------------- *)
+(* Geourjon Anthony / Savary Rémy - GR.2   *)
+(* TP6 Analyse syntaxique                  *)
 
-   Savary & Geourjon
-   17/17/15
+#load "dynlink.cma";;
+#load "camlp4o.cma";;
 
- *)
+(* Exercice 79 *)
 
-#load "dynlink.cma"
-#load "camlp4o.cma"
+let digit a = int_of_char a - int_of_char '0';;
+
+let rec horner (x : int)( s : char Stream.t ) : int =
+  match s with parser
+  | [<''0'..'9' as a >] -> horner (10*x + digit a) s
+  | [<>] -> x;;
+
+let s = "123 181";;
+horner 0 (Stream.of_string s);;
+horner 42 (Stream.of_string s);; 
+
+(* Exercice 80 *)
+
+type couleur = Nil | Blanc | Noir;; 
+type coup = Coup of couleur*int*int;;
+type plateau = Nil | Plateau of coup*plateau;;
+
+(* Exercice 81 *)
+
+type token = ParO | ParF | Int of int | Couleur of couleur;;
+
+let rec annalex_partie (s:char Stream.t): token Stream.t =
+  match s with parser
+  | [<''(';s = annalex_partie>] -> [<'ParO;s>]
+  | [<'')';s = annalex_partie>] -> [<'ParF;s>]
+  | [<''b';''l';''a';''n';''c';s=annalex_partie>] -> [<'Couleur(Blanc);s>]
+  | [<''n';''o';''i';''r';s=annalex_partie>] -> [<'Couleur(Noir);s>]
+  | [<''0'..'9' as c;i=horner(digit c);s=annalex_partie>] -> [<'Int(i);s>]
+  | [<'' '| '\n'| '\t' ;s=annalex_partie>] -> s
+  | [<>] -> [<>];;
 
 
-let digit c = int_of_char c - int_of_char '0'      
+let fichierPartieConnect6 = "(19 19)(noir 3 5) (blanc 5      8 )    (blanc 5  9)  (        noir 3 4)(noir 3 6)  ";;
+let maPartie = annalex_partie (Stream.of_string fichierPartieConnect6);; 
 
-let rec horner n = parser
-  | [< ''0'..'9' as c ; n =  horner (10 * n + digit c)>] ->  n
-  | [< >] -> n;;
+(* Exercice 82 *)
 
-let s = "123 1981";
-  
-horner 0 s;;  
+let rec sub_lit_partie (s:token Stream.t) : plateau =
+  match s with parser
+  | [< 'ParO; 'Couleur(couleur); 'Int(x); 'Int(y); 'ParF; s = sub_lit_partie >] -> Plateau(Coup(couleur,x,y),s)
+  | [<>] -> Nil
+  | [<'_>]-> failwith "Flux non valide";;
+
+let rec lit_partie  (s:token Stream.t) : (int*int)*plateau =
+  match s with parser
+  | [< 'ParO; 'Int(x); 'Int(y); 'ParF; s = sub_lit_partie >] -> ((x,y),s)
+  | [<>]->((0,0),Nil)
+  | [<'_>]-> failwith "Flux non valide";;
+
+
+let (taillePlateau, lesCoups) = lit_partie (annalex_partie (Stream.of_string fichierPartieConnect6));;
+
+(* Exercice 83 *)
+
+let rec joue_coup (plateau:plateau)(coup:coup) =
+  match plateau with
+  | Nil -> Plateau(coup,Nil)
+  | Plateau(coup1,plateau1) -> Plateau(coup1,(joue_coup plateau1 coup));;
+
+let monCoup = Coup(Noir,12,12);;
+
+joue_coup lesCoups monCoup;;
+
